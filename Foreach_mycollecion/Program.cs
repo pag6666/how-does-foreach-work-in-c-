bc70e56c4deda0b133202306ff1fff9cae745043
pag6666 <110739163+pag6666@users.noreply.github.com>
2023-06-14 08:47:13 +0300

    Добавьте файлы проекта.

---
+++ Foreach_mycollecion/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+        Book a = new Book(1, 2, 3, 4, 5, 6, "sdds", 34.23d,22.23f,2,'s',"AbD");
+        foreach(var i in a)
+            Console.WriteLine(i);
+        
+        Console.ReadKey();
+        }
+
+    }
+class Book : IEnumerable<object>
+{
+    object[] _arr;
+    public Book(params object[]arr) 
+    {
+        _arr = new object[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+            _arr[i] = arr[i];
+    }
+    //
+    public IEnumerator<object> GetEnumerator() => new BookEnum(_arr);
+    IEnumerator IEnumerable.GetEnumerator() => (IEnumerator)GetEnumerator();
+    //
+    class BookEnum : IEnumerator<object>
+    {
+        object[] _mydata;
+        int index = 0;
+        public object Current => _mydata[index];
+        public BookEnum(object[]mydata) 
+        {
+            _mydata = new object[mydata.Length];
+            for (int i = 0; i < mydata.Length; i++)
+                _mydata[i] = mydata[i];
+        }
+        public bool MoveNext()
+        {
+            index++;
+            return (index<_mydata.Length);
+        }
+
+        public void Reset()
+        {
+            index = 0;
+        }
+
+        public void Dispose()
+        {
+           
+        }
+    }
+}
+
---
+++ Foreach_mycollecion/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Foreach_mycollecion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Foreach_mycollecion")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a3715160-6aff-4e0b-8327-54678fc6e92a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
